e1503e0536f633e55deb60da20963eab96339e56
maruf45 <armaruf450@gmail.com>
2023-01-14 21:12:57 +0600

    update the latest code

--- index.js
+++ index.js
@@ -1,48 +1,48 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const cors = require('cors');
+const cors = require("cors");
 const port = process.env.PORT || 5000;
-const { MongoClient, ServerApiVersion } = require('mongodb');
-require('dotenv').config();
-
+const { MongoClient, ServerApiVersion } = require("mongodb");
+require("dotenv").config();
 
 //middleware
-app.use(cors())
-app.use(express.json())
+app.use(cors());
+app.use(express.json());
 
-
-const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS }@cluster0.ltux5vg.mongodb.net/?retryWrites=true&w=majority`;
-const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.ltux5vg.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
 
 async function run() {
-    try{
-        const users = client.db('Craft-Connect').collection('users');
-        const usersPost = client.db('Craft-Connect').collection('usersPost');
-        app.get('/', (req, res) => {
-            res.send('Craft connect server is running..')
-        })
-        app.post('/usersPost', async(req, res) => {
-            const usersData = req.body;
-            const result = await usersPost.insertOne(usersData);
-            res.send(result);
-        })
-        app.get('/usersPost', async(req, res) => {
-            const query = {};
-            const result = await usersPost.find(query).toArray();
-            res.send(result);
-        })
-        app.post('/users', async(req ,res) => {
-            const user = req.body;
-            const result = await users.insertOne(user);
-            res.send(result);
-        })
-
-    }
-    finally{}
+  try {
+    const users = client.db("Craft-Connect").collection("users");
+    const usersPost = client.db("Craft-Connect").collection("usersPost");
+    app.get("/", (req, res) => {
+      res.send("Craft connect server is running..");
+    });
+    app.post("/usersPost", async (req, res) => {
+      const usersData = req.body;
+      const result = await usersPost.insertOne(usersData);
+      res.send(result);
+    });
+    app.get("/usersPost", async (req, res) => {
+      const query = {};
+      const result = await usersPost.find(query).toArray().reverse();
+      res.send(result);
+    });
+    app.post("/users", async (req, res) => {
+      const user = req.body;
+      const result = await users.insertOne(user);
+      res.send(result);
+    });
+  } finally {
+  }
 }
-run().catch(error => console.log(error))
-
+run().catch((error) => console.log(error));
 
 app.listen(port, (req, res) => {
-    console.log('Craft connect server is running..')
-})
+  console.log("Craft connect server is running..");
+});
