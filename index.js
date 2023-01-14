1eb70f5f5fc037a66d3fd94bc05cca60e965038c
hasansikdar <hasansikdar973@gmail.com>
2023-01-14 14:07:42 +0600

    post deleted added

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const cors = require('cors');
 const port = process.env.PORT  || 5000;
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config();
 
 
@@ -18,19 +18,32 @@ async function run() {
     try{
         const users = client.db('Craft-Connect').collection('users');
         const usersPost = client.db('Craft-Connect').collection('usersPost');
+
+
+        // home page get api
         app.get('/', (req, res) => {
             res.send('Craft connect server is running..')
         })
+
+        // post added
         app.post('/usersPost', async(req, res) => {
             const usersData = req.body;
             const result = await usersPost.insertOne(usersData);
             res.send(result);
         })
+        // all posts get
         app.get('/usersPost', async(req, res) => {
             const query = {};
             const result = await usersPost.find(query).toArray();
             res.send(result);
         })
+        // post delete
+        app.delete('/usersPost/:id', async(req, res) => {
+            const id = req.params.id;
+            const result = await usersPost.deleteOne({_id: ObjectId(id)});
+            res.send(result);
+        })
+
 
         // user created post
         app.post('/users', async(req ,res) => {
